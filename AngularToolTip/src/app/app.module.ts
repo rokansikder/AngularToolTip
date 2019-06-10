d73601576c734f3ac481d1f0239bacbd16d30c0d
Rokan Sikder <rokan08@gmail.com>
2019-06-09 21:43:28 -0400

    change querystringify

--- AngularToolTip/src/app/app.module.ts
+++ AngularToolTip/src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes, Route} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TooltipDirective } from './directive/tooltip.directive';
 import {GlobalModule} from './global/global.module';
+import { HomeComponent } from './Home/home/home.component';
+
+const appRoutes:Routes = [
+  {path:'', component:HomeComponent, pathMatch:'full'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    TooltipDirective
+    TooltipDirective,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
     GlobalModule.forRoot()
   ],
   providers: [],
